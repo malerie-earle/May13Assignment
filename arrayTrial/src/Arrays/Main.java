d57332b447e811e0c847a7f9d89d6c3a9ae1392c
Malerie Earle <141525464+malerie-earle@users.noreply.github.com>
2024-05-13 09:04:31 -0230

    Update Main.java

--- arrayTrial/src/Arrays/Main.java
+++ arrayTrial/src/Arrays/Main.java
@@ -2,6 +2,24 @@ package Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // create the object of the class
+        SingleDimensionArray singleDimensionArray = new SingleDimensionArray(8);
+        singleDimensionArray.insert(0, 1);
+        singleDimensionArray.insert(1, 2);
+        singleDimensionArray.insert(2, 3);
+        singleDimensionArray.insert(3, 4);
+        singleDimensionArray.insert(4, 5);
+        singleDimensionArray.insert(5, 3);
+        singleDimensionArray.insert(6, 4);
+        singleDimensionArray.insert(7, 5);
+        // print all numbers within the array
+        singleDimensionArray.traverse();
+        // print the array
+        System.out.println(singleDimensionArray);
+        // delete an item from the array
+        singleDimensionArray.delete(1);
+        System.out.println(singleDimensionArray);
+        // delete entire array
+        singleDimensionArray.deleteArray();
     }
 }
